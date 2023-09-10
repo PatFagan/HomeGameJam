446938982f623b38e4657bdaf48b3f8788eeb086
PatFagan <patrickfagan1586@gmail.com>
2023-09-10 18:25:43 -0400

    difficulty increases over time, new goblins

--- HomeGameJamProject/Assets/Scripts/GoblinAttacks.cs
+++ HomeGameJamProject/Assets/Scripts/GoblinAttacks.cs
@@ -7,6 +7,9 @@ public class GoblinAttacks : MonoBehaviour
     // movement variables
     public float moveSpeed;
 
+    // raycast start variables
+    public float width;
+
     // attack variables
     public GameObject meleeAttack;
     public float attackDistance;
@@ -50,7 +53,7 @@ public class GoblinAttacks : MonoBehaviour
     {
         int layerMask = 1 << 8;
 
-        Vector3 rayCastStart = new Vector3(transform.position.x - .5f, transform.position.y, 0f);
+        Vector3 rayCastStart = new Vector3(transform.position.x - width, transform.position.y, 0f);
         bool nearEnemy = Physics2D.Raycast(rayCastStart, Vector3.left, attackDistance + .75f, layerMask);
         // buffer between units
         bool buffer = Physics2D.Raycast(rayCastStart, Vector3.left, 1f);
